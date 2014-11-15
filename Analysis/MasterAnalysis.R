0fd5fd913906e5e9e6c3bd60a2388c9df47b40ea
Chris Muir <cdmuir@biodiversity.ubc.ca>
2014-11-15 11:08:03 -0800

    Worked on analysis, made growth rate ANOVA tables, figures correlating growth with latitude, made refs.bib file, and added some to methods section of ms.tex

--- Analysis/MasterAnalysis.R
+++ Analysis/MasterAnalysis.R
@@ -35,6 +35,7 @@
 ##	Population Environmental data
 #
 	# This needs to be updated with PRISM, other data, etc. Lat, lon should be correct though
+	# Populations are ordered alphabetically by site to match output from stats
 	popenv <- read.csv("Data/PopulationEnvData.csv")
 	
 #
@@ -50,11 +51,7 @@
 	# LLL Growth rate
 	# Using Absolute growth rates for now as this seems most sensible. Need to test that this correlates with final biomass size.
 	
-	# approach 1: stepAIC, no Family effect
-	fit1 <- stepAIC(lm(lll_AbsGrowth ~ Population * TempTrt * WaterTrt, data = data))
-	Anova(fit1, type = 2)
-
-	# approach 2: mixed model with family	
+	# Mixed model ANOVA with family as random factor fit using step down procedure
 	mm <- lmer(lll_AbsGrowth ~ Population * TempTrt * WaterTrt + (1|Family),
 		data = data)
 	fit1 <- step(mm) #includes pop, temp, h2o, population x water
@@ -82,18 +79,46 @@
 	exportTable(file = "ms/Tables/Table_lllGrowthAnova.txt", data = dat, 
 		colnames = colnames, rownames = rownames)
 			
-	# Plot latitudinal variation
+	# Plot latitudinal variation in Intercept and Population:WaterTrt
 	
-	#### NEED TO DO ####
-
+	# best fit model based on stepdown elimination procedure
+	fit1 <- lmer(lll_AbsGrowth ~ Population + TempTrt + WaterTrt + Population:WaterTrt +
+		(1|Family), data = data)
+	
+	# Intercept: least-square coefficients ('betas') and 95% CIs
+	betas <- lsmeans(fit1)
+	
+	# Rows with Population coefficients
+	x <- grep("Population  [a-zA-Z]+", rownames(betas$lsmeans.table))
+	pdf("ms/Figures/Figure_LLL_Lat.pdf", 5, 5)
+	par(mar = c(5, 5, 1, 1))
+	plot(popenv$Lat, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), ylim = c(1.5, 3.1),
+		xlab = "Latitude of Origin", ylab = expression(paste("Leaf elongation rate (", 
+		mm~day^{-1}, ")")), las = 1, cex.lab = 1.5, type = "n", axes = F, frame.plot = T)
+	arrows(popenv$Lat, betas$lsmeans.table[x, "Lower CI"], popenv$Lat, 
+		betas$lsmeans.table[x, "Upper CI"], angle = 90, length = 0.05, code = 3)
+	points(popenv$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
+		pch = 21, cex = 1.5)
+	
+	axis(1, at = seq(30, 45, 5), labels = expression(30*degree, 35*degree, 40*degree,
+		45*degree), lwd = 0, lwd.ticks = 1)	
+	axis(2, lwd = 0, lwd.ticks = 1, las = 1)	
+	
+	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Lat, data = popenv)
+	points(range(popenv$Lat), predict(fit, new = data.frame(Lat = range(popenv$Lat))),
+		type = "l", lwd = 2)
+	
+	text(grconvertX(0.95, "npc"), grconvertY(0.95, "npc"), 
+		labels = bquote(italic(P) == .(round(summary(fit)[[4]][2, 4], 3))), adj = c(1, 1))
+	
+	dev.off()
+			
+	# Plasticity: difference in least-square coefficients ('dbetas') and 95% CIs
+	dbetas <- difflsmeans(fit1)
+	
 	# Height Growth rate
-	# Using Absolute growth rates for now as this seems most sensible. Need to test that this correlates with final biomass size.
 
-	# approach 1: stepAIC, no Family effect
-	fit1 <- stepAIC(lm(height_AbsGrowth ~ Population * TempTrt * WaterTrt, data = data))
-	Anova(fit1, type = 2)
-
-	# approach 2: mixed model with family	
+	# Mixed model ANOVA with family as random factor fit using step down procedure
 	mm <- lmer(height_AbsGrowth ~ Population * TempTrt * WaterTrt + (1|Family),
 		data = data)
 	fit1 <- step(mm, reduce.random = F) #includes pop, temp, h2o, temp x water
@@ -121,6 +146,41 @@
 	exportTable(file = "ms/Tables/Table_heightGrowthAnova.txt", data = dat, 
 		colnames = colnames, rownames = rownames)
 
+	# Plot latitudinal variation in Intercept and Population:WaterTrt
+	
+	# best fit model based on stepdown elimination procedure
+	fit1 <- lmer(height_AbsGrowth ~ Population + TempTrt + WaterTrt + TempTrt:WaterTrt +
+		(1|Family), data = data)
+	
+	# Intercept: least-square coefficients ('betas') and 95% CIs
+	betas <- lsmeans(fit1)
+	
+	# Rows with Population coefficients
+	x <- grep("Population  [a-zA-Z]+", rownames(betas$lsmeans.table))
+	pdf("ms/Figures/Figure_Height_Lat.pdf", 5, 5)
+	par(mar = c(5, 5, 1, 1))
+	plot(popenv$Lat, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), 
+		ylim = c(0.75, 1.65), xlab = "Latitude of Origin", 
+		ylab = expression(paste("Stem elongation rate (", mm~day^{-1}, ")")), cex.lab = 1.5,
+		type = "n", axes = F, frame.plot = T)
+	arrows(popenv$Lat, betas$lsmeans.table[x, "Lower CI"], popenv$Lat, 
+		betas$lsmeans.table[x, "Upper CI"], angle = 90, length = 0.05, code = 3)
+	points(popenv$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
+		pch = 21, cex = 1.5)
+	
+	axis(1, at = seq(30, 45, 5), labels = expression(30*degree, 35*degree, 40*degree,
+		45*degree), lwd = 0, lwd.ticks = 1)	
+	axis(2, lwd = 0, lwd.ticks = 1, las = 1)	
+	
+	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Lat, data = popenv)
+	points(range(popenv$Lat), predict(fit, new = data.frame(Lat = range(popenv$Lat))),
+		type = "l", lwd = 2)
+	
+	text(grconvertX(0.95, "npc"), grconvertY(0.95, "npc"), 
+		labels = bquote(italic(P) == .(round(summary(fit)[[4]][2, 4], 3))), adj = c(1, 1))
+	
+	dev.off()
+			
 
 
 
