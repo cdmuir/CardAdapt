376f5a8dc995695f5172bc1e1ede340ab45e5594
Chris Muir <cdmuir@biodiversity.ubc.ca>
2015-04-21 15:15:05 -0700

    Updated Analysis folder

--- Analysis/EnvironmentalAnalysis.R
+++ Analysis/EnvironmentalAnalysis.R
@@ -1,9 +1,19 @@
 library(raster)
 library(rgdal)
+library(geoR)
+
+# notes from discussion with Amy 12/17
+# two things:
+#1. Why is latitude a better predictor?
+#	a. maybe it's some multivariate thing (see if climate PCs are better than lat or single cliamte var alone)
+#	b. maybe need different time averages
+
+#2. Average selective environment
+#	
 
 ### Occurences
 	# Import
-	occCard <- read.csv("~/Google Drive/cardinalisENM/data files/herb.dat.lnpreds.csv")
+	occCard <- read.csv("~/Google Drive/cardinalisENM/data files/all.records.aug.31.csv")
 	
 	# Convert to SpatialPoints and exclude absences
 	occCard <- SpatialPoints(coords = occCard[occCard$PRESABS == 1, c("Longitude", 
@@ -11,24 +21,83 @@ library(rgdal)
 	
 	# Reproject to match raster layers
 	occCard <- spTransform(occCard, CRSobj = CRS("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
-	
+
 ### 8 environmental layers used in modeling
-	
-	# This doesn't work because file path is wrong
-	# load("/Users/cdmuir/Google Drive/cardinalisENM/R objects/wc0.5/pred.dom.Rda")
-	
-	# This recreates the same thing with correct local path
-	pred.dom <- stack(paste("~/Google Drive/cardinalisENM/R objects/wc0.5/", c("bio2.grd", 
-		"bio3.grd", "bio4.grd", "bio10.grd", "bio11.grd", "bio12.grd", "bio14.grd", 
-		"bio15.grd"), sep = ""))
+	### GET 8 LAYERS FOR FOCAL POPS!
+	pred <- stack(paste("~/Google Drive/CardLocalAdaptation/Data/Climate/Raw bios (untransformed)/", c("bio2.grd", "bio3.grd", "bio4.grd", "bio10.grd", "bio11.grd", "bio12.grd", "bio14.grd", "bio15.grd"), sep = ""))
 
+	logPred <- stack(paste("~/Google Drive/CardLocalAdaptation/Data/Climate/Predictor set (log transformed)/", c("bio2.grd", "bio3.grd", "bio4.grd", "bio10.grd", "bio11.grd", "bio12.grd", "bio14.grd", "bio15.grd"), sep = ""))
+
+### Get 8 layers for focal sites
+	popenv <- read.csv("~/Google Drive/CardLocalAdaptation/Data/PopulationEnvData.csv")
+
+	# Convert to SpatialPoints
+	tmp <- SpatialPoints(coords = popenv[, c("Lon", "Lat")], proj4string = CRS("+proj=longlat +ellps=WGS84"))
+
+	# Reproject to match raster layers
+	tmp <- spTransform(tmp, CRSobj = CRS("+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+
+	extract(pred, tmp, method = "bilinear")
+	extract(logPred, tmp, method = "bilinear")
 ### ENM predicted suitability
-
-	load("~/Google Drive/cardinalisENM/R objects/RF.prob.ave.img")
-
-### Overlay to get predictors weighted by suitability
 	
-	pred.dom.wt <- overlay(pred.dom, RF.prob.ave, fun = function(x, y) {(x * y)})
+	# Note this currently just from Random Forest model - maybe better to use consensus model or whatever?
+	load("~/Google Drive/cardinalisENM/R objects/RF.prob.ave.img")
+	# use suitability as weight?
+	wts <- extract(RF.prob.ave, occCard)
+
+### PC of 8 climate variables at sites
+	climPC <- prcomp(predCard, retx = TRUE, center = TRUE, scale. = TRUE)
+
+
+###	
+### 1. Is latitude complex composite of environmental factors?
+###
+library(e1071)
+occCard <- read.csv("~/Google Drive/cardinalisENM/data files/all.records.aug.31.csv")
+absCard <- subset(occCard, occCard$PRESABS == 0)
+absCard <- subset(absCard, select = c("Latitude", "Elevation", "bio3", "bio4", "bio10", "bio11", "bio12", "bio14", "bio15"))
+
+occCard <- subset(occCard, occCard$PRESABS == 1)
+occCard <- subset(occCard, select = c("Latitude", "Elevation", "bio3", "bio4", "bio10", "bio11", "bio12", "bio14", "bio15"))
+
+# Training set
+trainset <- sample(1:nrow(occCard), floor(nrow(occCard) / 2))
+testset <- c(1:nrow(occCard))[-trainset]
+
+fit <- svm(Latitude ~ Elevation + bio3 + bio4 + bio10 + bio11 + bio12 + bio14 + bio15, data = occCard[trainset, ])
+
+with(occCard[trainset, ], plot(Latitude, predict(fit)))
+with(occCard[trainset, ], cor(Latitude, predict(fit)))
+with(occCard[testset, ], plot(Latitude, predict(fit, newdata = occCard[testset, -1])))
+with(occCard[testset, ], cor(Latitude, predict(fit, newdata = occCard[testset, -1])))
+
+with(absCard, plot(Latitude, predict(fit, newdata = absCard)))
+with(absCard, cor(Latitude, predict(fit, newdata = absCard)))
+
+# could test whether this is nonrandom by reperforming with latitudes randomly permuted
+
+occCard$Latitude <- sample(occCard$Latitude, nrow(occCard))
+fit <- svm(Latitude ~ Elevation + bio3 + bio4 + bio10 + bio11 + bio12 + bio14 + bio15, data = occCard[trainset, ])
+
+with(occCard[trainset, ], plot(Latitude, predict(fit)))
+with(occCard[trainset, ], cor(Latitude, predict(fit)))
+with(occCard[testset, ], plot(Latitude, predict(fit, newdata = occCard[testset, -1])))
+with(occCard[testset, ], cor(Latitude, predict(fit, newdata = occCard[testset, -1])))
+
+
+###	
+### 2. Does average selective environment change with latitude?
+###
+	predCard <- extract(pred.dom, occCard)
+	plot(occCard$Latitude, predCard[, 1])
+	plot(occCard$Latitude, predCard[, 2])
+	plot(occCard$Latitude, predCard[, 3])
+	plot(occCard$Latitude, predCard[, 4]) # temp of warmest quarter
+	plot(occCard$Latitude, predCard[, 5])
+	plot(occCard$Latitude, predCard[, 6])
+	plot(occCard$Latitude, predCard[, 7])
+	plot(occCard$Latitude, predCard[, 8])
 
 ### For every presence, calculate nearby spatial variation (coef of var) in suitability
 	tmp <- extract(RF.prob.ave, occCard, buffer = 1e4)
@@ -40,4 +109,55 @@ library(rgdal)
 	suitBio1 <- sapply(tmp, function(X) var(X, na.rm = T) / mean(X, na.rm = T))
 	plot(occCard$Latitude, suitBio1, log = "y")
 	fit <- lm(log(suitBio1) ~ occCard$Latitude)
+
+# Variogram or something
 	
+0.00057485 for buffer = 1e3
+	semivarProb <- function(prob, coord, buffer)
+	{
+		# suitability of occurence points
+		probOcc <- extract(prob, coord)
+
+		# suitability of buffer of 1 km
+		probBuff <- extract(prob, coord, buffer = buffer)
+
+		# remove NAs
+		probBuff <- lapply(probBuff, function(X) X[!is.na(X)])
+	
+		# remove focal suitability from buffer
+		probBuff <- mapply(function(X, Y) X[-which(X == Y)[1]], probBuff, probOcc)
+
+		# calculate semivariance
+		out <- mapply(function(X, Y) (1 / (2 * length(X))) * sum((X - Y) ^ 2), probBuff, 
+			probOcc)
+		
+		return(out)
+	}
+
+dists <- seq(1e3, 1e5, l = 1e2)
+svRF_south <- sapply(dists, function(X) semivarProb(RF.prob.ave, south, buffer = X))
+svRF_center <- sapply(dists, function(X) semivarProb(RF.prob.ave, center, buffer = X))
+svRF_north <- sapply(dists, function(X) semivarProb(RF.prob.ave, north, buffer = X))
+plot(dists, apply(svRF_south, 2, median, na.rm = T), type = "l", ylim = c(0, 0.05))
+points(dists, apply(svRF_center, 2, median, na.rm = T), type = "l")
+points(dists, apply(svRF_north, 2, median, na.rm = T), type = "l")
+
+for (i in 1:nrow(svRF)) 
+plot(dists, svRF[16, ], type = "l", col = "grey", log = "x")
+# do by region...
+occCard$Latitude
+
+
+
+file <- system.file("external/spain.grd", package="usdm")
+ 
+r <- raster(ncols = 100, nrows = 100)
+r[] <- rnorm(1e4) 
+plot(r) # plot the first RasterLayer in r
+ 
+v1 <- Variogram(r) # compute the sample variogram for the first layer in r
+ 
+v2 <- Variogram(r, cutoff = 30, lag = 2) # specify the lag and cutoff parameters
+plot(v2, cloud = T)
+
+RF.prob.ave[1:10, 1:15]
--- Analysis/MasterAnalysis.R
+++ Analysis/MasterAnalysis.R
@@ -1,10 +1,15 @@
 #
 #	Master Analysis of M. cardinalis Growth Chamber Experiment (Spring 2014)
 #
-#	Title:	 Local Physiological adaptaiton in Mimulus cardinalist
+#	Title:	 Local Physiological adaptaiton in Mimulus cardinalis
 #	Authors: Christopher D. Muir and Amy L. Angert
 #	Journal: Evolution?
 #
+
+
+## PART 1:	TRAIT VARIATION
+## PART 2: 	SELECTIVE AGENT
+## PART 3	???
 
 ### Preliminaries
 
@@ -19,6 +24,7 @@
 	library(pbkrtest)
 	library(lmerTest)
 	library(survival)
+	library(VSURF)
 	
 #
 ##	Set working directory
@@ -33,11 +39,11 @@
 	source("Analysis/functions.R")
 	
 #
-##	Population Environmental data
+##	Focal population climate data
 #
-	# This needs to be updated with PRISM, other data, etc. Lat, lon should be correct though
+	# This data.frame will change as climate data pipeline is updated
 	# Populations are ordered alphabetically by site to match output from stats
-	popenv <- read.csv("Data/PopulationEnvData.csv")
+	load('~/Google Drive/CardLocalAdaptation/Data/Climate/Robjects/focClim.RData')
 	
 #
 ##	Read in processed datasets
@@ -54,32 +60,35 @@
 	germ <- with(subset(data, !is.na(data$MinGermDay)), Surv(MinGermDay, MaxGermDay,	
 		type = "interval2"))
 
-	# lognormal produced best fit to data
+	# lognormal produced best fit to data (ID = Population effect)
 	fit1 <- survreg(germ ~ Population + frailty(Family, sparse = F), 
 		data = subset(data, !is.na(data$MinGermDay)), dist = "lognormal")
 	anova(fit1) # significant family and population effects
 	
+	# Add population germination effects to climate data.frame for subsequent analysis
+	focClim$DoG <- c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16])
+	
 	# Figure of germination time by latitude
-	png("ms/Figures/Figure_DoG_Lat.png", 5, 5, units = "in", res = 1e3)
+	pdf("ms/Figures/Figure_DoG_Lat.pdf", 5, 5)
 	par(mar = c(5, 5, 1, 1))
-	plot(popenv$Lat, c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]), xlim = c(30, 45), 
-		ylim = c(1.65, 2.65), xlab = "Latitude of Origin", 
+	plot(focClim$Latitude, c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]), 
+		xlim = c(30, 45), ylim = c(1.65, 2.65), xlab = "Latitude of Origin", 
 		ylab = "Day of Germination [log scale]", 
 		cex.lab = 1.5, type = "n", axes = F, frame.plot = T)
 	lowCI <- c(confint(fit1)[1, 1], coef(fit1)[1] + confint(fit1)[2:16, 1])
 	uppCI <- c(confint(fit1)[1, 2], coef(fit1)[1] + confint(fit1)[2:16, 2])
-	arrows(popenv$Lat, lowCI, popenv$Lat, uppCI, length = 0.05, angle = 90, code = 3)
-	points(popenv$Lat, c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]), col = "white",
-		bg = "black", pch = 21, cex = 1.5)
+	arrows(focClim$Latitude, lowCI, focClim$Lat, uppCI, length = 0.05, angle = 90, code = 3)
+	points(focClim$Latitude, c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]), 
+		col = "white", bg = "black", pch = 21, cex = 1.5)
 	
 	axis(1, at = seq(30, 45, 5), labels = expression(30*degree, 35*degree, 40*degree,
 		45*degree), lwd = 0, lwd.ticks = 1)	
 	axis(2, lwd = 0, lwd.ticks = 1, las = 1, at = log(seq(6, 14, 2)),
 		labels = seq(6, 14, 2))	
 	
-	fit <- lm(c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]) ~ Lat, data = popenv)
-	points(range(popenv$Lat), predict(fit, new = data.frame(Lat = range(popenv$Lat))),
-		type = "l", lwd = 2)
+	fit <- lm(c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16]) ~ Latitude, data = focClim)
+	points(range(focClim$Latitude), predict(fit, new = data.frame(Latitude = 
+		range(focClim$Latitude))), type = "l", lwd = 2)
 	
 	text(grconvertX(0.05, "npc"), grconvertY(0.95, "npc"), adj = c(0, 1),
 		labels = bquote(italic(P) == .(round(summary(fit)[[4]][2, 4], 3))),)
@@ -210,15 +219,14 @@
 	exportTable(file = "ms/Tables/Table_InitialSize.txt", data = dat, 
 		colnames = colnames, rownames = rownames, h1 = "Model")
 
-### JUST USING GROWTH NOW UNTIL OTHER DATA ARE READY
-### Analysis 1: Variation in 'intrinsic' traits (significant main effect of Population)
-### Analysis 2: Variation in plasticity (significant Treatment x Population interaction)
+### Question 1: Variation in 'intrinsic' traits (significant main effect of Population)
+### Question 2: Variation in plasticity (significant Treatment x Population interaction)
 
-	# LLL Growth rate
+	# Leaf expansion rate (= LLL Growth rate)
 	# Using Absolute growth rates for now as this seems most sensible. Need to test that this correlates with final biomass size.
 	
 	# Mixed model ANOVA with family as random factor fit using step down procedure
-	mm <- lmer(lll_AbsGrowth ~ AvgGermDay + Population * TempTrt * WaterTrt + (1|Family),
+	mm <- lmer(lll_AbsGrowth ~ AvgGermDay + ID * TempTrt * WaterTrt + (1|Family),
 		data = data)
 	fit1 <- step(mm, reduce.random = F) #includes pop, temp, h2o, population x water
 	# fit1 <- step(mm, ddf = "Kenward-Roger", reduce.random = F) # same result using Satterthwaite ddf
@@ -249,44 +257,49 @@
 	# Plot latitudinal variation in Intercept
 	
 	# best fit model based on stepdown elimination procedure
-	fit1 <- lmer(lll_AbsGrowth ~ AvgGermDay + Population + TempTrt + WaterTrt +
-		Population:WaterTrt + (1|Family), data = data)
+	fit1 <- lmer(lll_AbsGrowth ~ AvgGermDay + ID + TempTrt + WaterTrt + ID:WaterTrt + 
+		(1|Family), data = data)
 		
 	# Intercept: least-square coefficients ('betas') and 95% CIs
 	betas <- lsmeans(fit1)
 	
 	# Rows with Population coefficients
-	x <- grep("Population  [a-zA-Z]+", rownames(betas$lsmeans.table))
+	x <- grep("ID  [a-zA-Z]+", rownames(betas$lsmeans.table))
+
+	# Add population leaf elongation rate to climate data.frame for subsequent analysis
+	focClim$LER <- betas$lsmeans.table$Estimate[x]
+
 	pdf("ms/Figures/Figure_LLL_Lat.pdf", 5, 5)
 	par(mar = c(5, 5, 1, 1))
-	plot(popenv$Lat, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), 
+	plot(focClim$Latitude, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), 
 		ylim = c(1.5, 3.1), xlab = "Latitude of Origin", 
 		ylab = expression(paste("Leaf elongation rate (", mm~day^{-1}, ")")), 
 		las = 1, cex.lab = 1.5, type = "n", axes = F, frame.plot = T)
-	arrows(popenv$Lat, betas$lsmeans.table[x, "Lower CI"], popenv$Lat, 
+	arrows(focClim$Lat, betas$lsmeans.table[x, "Lower CI"], focClim$Lat, 
 		betas$lsmeans.table[x, "Upper CI"], angle = 90, length = 0.05, code = 3)
-	points(popenv$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
+	points(focClim$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
 		pch = 21, cex = 1.5)
 	
 	axis(1, at = seq(30, 45, 5), labels = expression(30*degree, 35*degree, 40*degree,
 		45*degree), lwd = 0, lwd.ticks = 1)	
 	axis(2, lwd = 0, lwd.ticks = 1, las = 1)	
 	
-	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Lat, data = popenv)
-	points(range(popenv$Lat), predict(fit, new = data.frame(Lat = range(popenv$Lat))),
-		type = "l", lwd = 2)
-	
+	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Latitude, data = focClim)
+
+	points(range(focClim$Latitude), predict(fit, new = data.frame(Latitude = 
+		range(focClim$Latitude))), type = "l", lwd = 2)
+
 	text(grconvertX(0.95, "npc"), grconvertY(0.95, "npc"), adj = c(1, 1),
 		labels = bquote(italic(P) == .(round(summary(fit)[[4]][2, 4], 3))))
 	
 	dev.off()
 			
-	# Height Growth rate
+	# Stem elongation rate (= Height Growth rate)
 
 	# Mixed model ANOVA with family as random factor fit using step down procedure
 	mm <- lmer(height_AbsGrowth ~ AvgGermDay + Population * TempTrt * WaterTrt + 
 		(1|Family), data = data)
-	fit1 <- step(mm, reduce.random = F) #includes pop, temp, h2o, temp x water
+	fitSER <- step(mm, reduce.random = F) #includes pop, temp, h2o, temp x water
 	# fit2 <- step(mm, ddf = "Kenward-Roger", reduce.random = F) # same result using Satterthwaite ddf
 
 	# Dyanamic table of mm output for ms.tex
@@ -315,31 +328,35 @@
 	# Plot latitudinal variation in Intercept and Population:WaterTrt
 	
 	# best fit model based on stepdown elimination procedure
-	fit1 <- lmer(height_AbsGrowth ~ AvgGermDay + Population + TempTrt + WaterTrt + 
+	fit1 <- lmer(height_AbsGrowth ~ AvgGermDay + ID + TempTrt + WaterTrt + 
 		TempTrt:WaterTrt + (1|Family), data = data)
 
 	# Intercept: least-square coefficients ('betas') and 95% CIs
 	betas <- lsmeans(fit1)
 	
 	# Rows with Population coefficients
-	x <- grep("Population  [a-zA-Z]+", rownames(betas$lsmeans.table))
+	x <- grep("ID  [a-zA-Z]+", rownames(betas$lsmeans.table))
+
+	# Add population stem elongation rate to climate data.frame for subsequent analysis
+	focClim$SER <- betas$lsmeans.table$Estimate[x]
+
 	pdf("ms/Figures/Figure_Height_Lat.pdf", 5, 5)
 	par(mar = c(5, 5, 1, 1))
-	plot(popenv$Lat, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), 
+	plot(focClim$Lat, betas$lsmeans.table$Estimate[x], xlim = c(30, 45), 
 		ylim = c(0.75, 1.65), xlab = "Latitude of Origin", 
 		ylab = expression(paste("Stem elongation rate (", mm~day^{-1}, ")")), 
 		cex.lab = 1.5, type = "n", axes = F, frame.plot = T)
-	arrows(popenv$Lat, betas$lsmeans.table[x, "Lower CI"], popenv$Lat, 
+	arrows(focClim$Lat, betas$lsmeans.table[x, "Lower CI"], focClim$Lat, 
 		betas$lsmeans.table[x, "Upper CI"], angle = 90, length = 0.05, code = 3)
-	points(popenv$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
+	points(focClim$Lat, betas$lsmeans.table$Estimate[x], col = "white", bg = "black",
 		pch = 21, cex = 1.5)
 	
 	axis(1, at = seq(30, 45, 5), labels = expression(30*degree, 35*degree, 40*degree,
 		45*degree), lwd = 0, lwd.ticks = 1)	
 	axis(2, lwd = 0, lwd.ticks = 1, las = 1)	
 	
-	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Lat, data = popenv)
-	points(range(popenv$Lat), predict(fit, new = data.frame(Lat = range(popenv$Lat))),
+	fit <- lm(betas$lsmeans.table$Estimate[x] ~ Lat, data = focClim)
+	points(range(focClim$Lat), predict(fit, new = data.frame(Lat = range(focClim$Lat))),
 		type = "l", lwd = 2)
 	
 	text(grconvertX(0.95, "npc"), grconvertY(0.95, "npc"), adj = c(1, 1),
@@ -347,24 +364,171 @@
 	
 	dev.off()
 			
+  # Photosynthetic rate
+  mm <- lmer(Photo ~ Population * TempTrt * WaterTrt + (1|Family),
+    data = subset(data, !is.na(data$Photo)))
+	fitPhoto <- step(mm, reduce.random = F)
+  
+	mm <- lmer(resPhoto ~ Population * TempTrt * WaterTrt + (1|Family),
+	           data = subset(data, !is.na(data$Photo))[-52, ])
+	fitIntrnPhoto <- step(mm, reduce.random = F)
+  
+	# Morality
+	fitMort <- glmer(DiedFromStress ~ Population * TempTrt * WaterTrt + (1|Family) + (1|indiv),
+    data = subset(data, !is.na(data$DiedFromStress)), family = "binomial")
+
+  fitMort <- glm(DiedFromStress ~ Population * TempTrt * WaterTrt,
+	                 data = subset(data, !is.na(data$DiedFromStress)), family = "quasibinomial")
+
+  Anova(fitMort, type = 2)
+  
+  
+  #### TO DO: explain variation in plasticity...
+# PART II: SELECTIVE AGENT
+
+	# Load vector of exploratory climate variables used in climate to latitude model search
+	load("~/Google Drive/CardLocalAdaptation/Data/Climate/Robjects/Exploratory_climate_variables.RData") #climVars
+	
+	# Run VSURF
+	set.seed(022918)
+	linmod.DoG <- as.formula(paste("DoG ~ ", paste(climVars, collapse = " + "), sep = ""))
+	linmod.LER <- as.formula(paste("LER ~ ", paste(climVars, collapse = " + "), sep = ""))
+	linmod.SER <- as.formula(paste("SER ~ ", paste(climVars, collapse = " + "), sep = ""))
+	fitDoG <- VSURF.parallel(linmod.DoG, data = focClim[, c("DoG", climVars)], ncores = 3)
+	fitLER <- VSURF.parallel(linmod.LER, data = focClim[, c("LER", climVars)], ncores = 3)
+	fitSER <- VSURF.parallel(linmod.SER, data = focClim[, c("SER", climVars)], ncores = 3)
+	climVars[fitDoG$varselect.pred]
+	climVars[fitLER$varselect.pred]
+	climVars[fitSER$varselect.pred]
+	
+	# save(fitDoG, file = "Analysis/clim2DoG_fit.RData")
+	# save(fitLER, file = "Analysis/clim2LER_fit.RData")
+	# save(fitSER, file = "Analysis/clim2SER_fit.RData")
+	load(file = "Analysis/clim2DoG_fit.RData")
+	load(file = "Analysis/clim2LER_fit.RData")
+	load(file = "Analysis/clim2SER_fit.RData")
+	
+	# Figure for comparison climate 2 latitude analysis
+	
+	load(file = paste(path.obj, "/clim2lat_fit.RData", sep = "")) # fit
+	load(file = paste(path.obj, "/clim2lat_fit_t1.RData", sep = "")) # fit_t1
+	
+	# Important climatic variables for predicting latitude
+	impVars <- climVars[fit_t1$varselect.pred]
+	nImpLat <- length(fit_t1$varselect.pred)
+	nImpDoG <- length(fitDoG$varselect.pred)
+	nImpLER <- length(fitLER$varselect.pred)
+	nImpSER <- length(fitSER$varselect.pred)
+	
+	# Figure of variable importance
+	pdf("~/Google Drive/CardLocalAdaptation/ms/Figures/Figure_clim2traits_imp.pdf", 4, 5)
+	par(mar = c(5, 1, 1, 6))
+	
+	# Day of Germination
+	plot(0, 0, type = "n", xlim = c(0, fitDoG$ord.imp$x[1] * 1000), ylim = c(1, nImpLat), 
+		axes = F, frame.plot = T, xlab = "Importance", ylab = "", cex.lab = 1.5)
+	for (i in (nImpLat:(nImpLat - nImpDoG + 1))) lines(x = c(-100, 100), y = c(i, i), 
+		col = "grey")
+	points(1000 * fitDoG$ord.imp$x[1:nImpDoG], nImpLat:(nImpLat - nImpDoG + 1), pch = 21, 
+		col = "black", bg = "white", cex = 1.5)
+	mtext(climVars[fitDoG$varselect.pred], side = 4, at = nImpLat:(nImpLat - nImpDoG + 1), 
+		las = 1, line = 1, font = ifelse(fitDoG$varselect.pred %in% fit_t1$varselect.pred,
+		2, 1))
+	axis(1, lwd = 0, lwd.ticks = 1, labels = seq(0, 2, 0.5) / 1e3, at = seq(0, 2, 0.5))
+
+	# Leaf expansion rate
+	plot(0, 0, type = "n", xlim = c(0, fitLER$ord.imp$x[1] * 1000), ylim = c(1, nImpLat), 
+		axes = F, frame.plot = T, xlab = "Importance", ylab = "", cex.lab = 1.5)
+	for (i in (nImpLat:(nImpLat - nImpLER + 1))) lines(x = c(-100, 100), y = c(i, i), 
+		col = "grey")
+	points(1000 * fitLER$ord.imp$x[1:nImpLER], nImpLat:(nImpLat - nImpLER + 1), pch = 21, 
+		col = "black", bg = "white", cex = 1.5)
+	mtext(climVars[fitLER$varselect.pred], side = 4, at = nImpLat:(nImpLat - nImpLER + 1), 
+		las = 1, line = 1, font = ifelse(fitLER$varselect.pred %in% fit_t1$varselect.pred,
+		2, 1))
+	axis(1, lwd = 0, lwd.ticks = 1, labels = seq(0, 1.5, 0.5) / 1e3, at = seq(0, 1.5, 0.5))
+
+	# Stem elongation rate
+	plot(0, 0, type = "n", xlim = c(0, fitSER$ord.imp$x[1] * 1000), ylim = c(1, nImpLat), 
+		axes = F, frame.plot = T, xlab = "Importance", ylab = "", cex.lab = 1.5)
+	for (i in (nImpLat:(nImpLat - nImpSER + 1))) lines(x = c(-100, 100), y = c(i, i), 
+		col = "grey")
+	points(1000 * fitSER$ord.imp$x[1:nImpSER], nImpLat:(nImpLat - nImpSER + 1), pch = 21, 
+		col = "black", bg = "white", cex = 1.5)
+	mtext(climVars[fitSER$varselect.pred], side = 4, at = nImpLat:(nImpLat - nImpSER + 1), 
+		las = 1, line = 1, font = ifelse(fitSER$varselect.pred %in% fit_t1$varselect.pred,
+		2, 1))
+	axis(1, lwd = 0, lwd.ticks = 1, labels = seq(0, 3, 0.5) / 1e3, at = seq(0, 3, 0.5))
+
+	dev.off()
+	
+	
+#### code for multimodal avg if I decide to use this:
+
+library("MuMIn")
+
+# test all two parameter combinations for germination (DoG), leaf elongation rate (LER), and stem elongation rate (SER), and do multimodel average
+focClim$DoG <- c(coef(fit1)[1], coef(fit1)[1] + coef(fit1)[2:16])
+focClim$LER <- betas$lsmeans.table$Estimate[x]
+focClim$SER <- betas$lsmeans.table$Estimate[x]
+
+# should latitude be included in predictors???
+predCombos <- combn(c(2, 6:57), 2)
+par2Formulae <- apply(predCombos, 2, function(X)
+{
+	as.formula(paste("DoG ~ ", paste(colnames(focClim)[X], collapse = " + "), sep = ""))
+})
+
+fitDoG <- lapply(par2Formulae, lm, data = focClim)
+
+bestModel <- which.min(sapply(fitDoG, AIC))
+par2Formulae[[bestModel]]
+summary(fitDoG[[bestModel]])
+
+# models with delta(AIC) < 2
+dAIC <- sapply(fitDoG, AIC) - min(sapply(fitDoG, AIC))
+par2Formulae[[which(dAIC <= 2)[1]]]
 
 
+#####
+
+par2Formulae <- apply(predCombos, 2, function(X)
+{
+	as.formula(paste("LER ~ ", paste(colnames(focClim)[X], collapse = " + "), sep = ""))
+})
+
+fitLER <- lapply(par2Formulae, lm, data = focClim)
+
+bestModel <- which.min(sapply(fitLER, AIC))
+par2Formulae[[bestModel]]
+summary(fitLER[[bestModel]])
+
+# models with delta(AIC) < 2
+dAIC <- sapply(fitLER, AIC) - min(sapply(fitLER, AIC))
+par2Formulae[[which(dAIC <= 2)[3]]]
+
+# multimodel inference
+mmAvg <- model.sel(fitLER)
+importance(mmAvg)
 
 
+#####
 
-#
-# DFA/PCA
-#
+par2Formulae <- apply(predCombos, 2, function(X)
+{
+	as.formula(paste("SER ~ ", paste(colnames(focClim)[X], collapse = " + "), sep = ""))
+})
 
-	fit <- lda(Population ~ b0_lll + b1_height,
-		data = subset(data, data$TempTrt == "Hot"), CV = T)
-	ct <- table(data$Population[data$TempTrt == "Hot"], fit$class)
-	diag(prop.table(ct, 1))
-	# total percent correct
-	sum(diag(prop.table(ct)))
+fitSER <- lapply(par2Formulae, lm, data = focClim)
 
-# scratch
+bestModel <- which.min(sapply(fitSER, AIC))
+par2Formulae[[bestModel]]
+summary(fitSER[[bestModel]])
 
-tapply(lll.data$LLL, lll.data$indiv, max)
-plot(ranef(mm)$indiv[, 1], tapply(lll.data$LLL, lll.data$indiv, max))
-plot(ranef(mm)$indiv[, 3], tapply(height.data$height, height.data $indiv, max))
+# models with delta(AIC) < 2
+dAIC <- sapply(fitSER, AIC) - min(sapply(fitSER, AIC))
+par2Formulae[[which(dAIC <= 2)[1]]]
+
+# multimodel inference
+mmAvg <- model.avg(fitSER)
+importance(mmAvg)
